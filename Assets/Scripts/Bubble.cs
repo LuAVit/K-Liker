67a95e5cae814ef3054d36f4a83b83c0fb755203
LuAVit <52428028+LuAVit@users.noreply.github.com>
2019-07-02 00:17:05 +0300

    Add files via upload

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _scaleSpeed;
+    [SerializeField] private float _maxScale;
+
+    private void FixedUpdate()
+    {
+        if (transform.localScale.x <= _maxScale)
+        {
+            transform.position = Vector3.Lerp(transform.position,
+                new Vector3(transform.position.x, transform.position.y + _speed, transform.position.z), Time.deltaTime);
+            transform.localScale = Vector3.Lerp(transform.localScale,
+                new Vector3(transform.localScale.x + _scaleSpeed, transform.localScale.y + _scaleSpeed,
+                    transform.localScale.z + _scaleSpeed), Time.deltaTime);
+
+        }
+        else
+        {
+            transform.position = new Vector3(Random.Range(-7.0f, 7.0f), 0, 10);
+            transform.localScale = new Vector3(1,1,1);
+        }
+    }
+
+}
---
+++ Assets/Scripts/RayCastHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RayCastHelper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    [SerializeField] public Camera cam;
+    [SerializeField] private GameObject cubik;
+    public int score;
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        
+        //     RaycastHit hit = Physics.Raycast(ray.origin, ray.direction);
+        //  RaycastHit hit;
+        // Physics.Raycast(ray, out hit);
+   
+          if (Input.GetMouseButtonDown(0))
+          {
+        
+        
+        
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 50, Color.yellow);
+        RaycastHit hit = new RaycastHit();
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(ray, 20))
+        {
+            print("Collided !!!");
+            print(score);
+ //           print( hit.collider.gameObject.tag);
+ //           if (hit.collider.gameObject.tag == "Cubik")
+   //             Destroy(cubik);
+            score = score + 1;
+            GameObject.FindGameObjectWithTag ("ScoreCounter").GetComponent<Text>().text = "Score: " + score.ToString();
+            
+        }
+      
+        }
+        
+    }
+}
