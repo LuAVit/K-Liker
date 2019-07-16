9f037f27d88d7b7251709dfa76a59dca47444812
LuAVit <52428028+LuAVit@users.noreply.github.com>
2019-07-17 01:25:21 +0300

    Add files via upload

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -7,10 +7,65 @@ public class Bubble : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _scaleSpeed;
     [SerializeField] private float _maxScale;
+    [SerializeField] private int _bubbleLifetime;
+    private int _timeCounter = 0;
 
     private void FixedUpdate()
     {
-        if (transform.localScale.x <= _maxScale)
+
+
+        if (_timeCounter <= _bubbleLifetime)
+
+        {
+          //  print(_timeCounter);
+            if (_timeCounter >= _bubbleLifetime - 3)
+                _timeCounter = 0;
+
+            _timeCounter = _timeCounter + 1;
+        }
+
+        // else
+       // {
+       //    _timeCounter = _timeCounter - _bubbleLifetime;
+       //     print(_timeCounter);
+       // }
+        
+ //       if (transform.localScale.x <= _maxScale)
+            if (RayCastHelper.Instance.hitCounts <= 3)
+               
+        {
+            if (transform.localScale.x <= _maxScale)
+            {
+                transform.position = Vector3.Lerp(transform.position,
+                    new Vector3(transform.position.x, transform.position.y + _speed, transform.position.z),
+                    Time.deltaTime);
+                transform.localScale = Vector3.Lerp(transform.localScale,
+                    new Vector3(transform.localScale.x + _scaleSpeed, transform.localScale.y + _scaleSpeed,
+                        transform.localScale.z + _scaleSpeed), Time.deltaTime);
+
+            }
+            else
+            {
+                transform.position = new Vector3(Random.Range(-4.0f, 4.0f), 0, 10);
+                transform.localScale = new Vector3(1,1,1);
+                
+            }
+
+
+        }
+        else
+        {
+            transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-2.0f, 2.0f), 10);
+            transform.localScale = new Vector3(1,1,1);
+        }
+    }
+
+
+
+
+   
+  /* 
+        if (_timeCounter <= _bubbleLifetime)
         {
             transform.position = Vector3.Lerp(transform.position,
                 new Vector3(transform.position.x, transform.position.y + _speed, transform.position.z), Time.deltaTime);
@@ -24,6 +79,6 @@ public class Bubble : MonoBehaviour
             transform.position = new Vector3(Random.Range(-7.0f, 7.0f), 0, 10);
             transform.localScale = new Vector3(1,1,1);
         }
-    }
-
+    
+*/
 }
--- Assets/Scripts/RayCastHelper.cs
+++ Assets/Scripts/RayCastHelper.cs
@@ -10,10 +10,18 @@ public class RayCastHelper : MonoBehaviour
     {
         
     }
+    public static RayCastHelper Instance { get; private set; }
+    
     [SerializeField] public Camera cam;
     [SerializeField] private GameObject cubik;
-    public int score;
+    public float score;
+    public float hitCounts;
     // Update is called once per frame
+    
+    public void Awake()
+    {
+        Instance = this;
+    }
     void Update()
     {
 
@@ -22,30 +30,43 @@ public class RayCastHelper : MonoBehaviour
         //     RaycastHit hit = Physics.Raycast(ray.origin, ray.direction);
         //  RaycastHit hit;
         // Physics.Raycast(ray, out hit);
-   
-          if (Input.GetMouseButtonDown(0))
-          {
+       if (hitCounts >= 4)
         
-        
-        
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(ray.origin, ray.direction * 50, Color.yellow);
-        RaycastHit hit = new RaycastHit();
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+            hitCounts = 0;
+       else
+       {
 
-        if (Physics.Raycast(ray, 20))
-        {
-            print("Collided !!!");
-            print(score);
- //           print( hit.collider.gameObject.tag);
- //           if (hit.collider.gameObject.tag == "Cubik")
-   //             Destroy(cubik);
-            score = score + 1;
-            GameObject.FindGameObjectWithTag ("ScoreCounter").GetComponent<Text>().text = "Score: " + score.ToString();
-            
-        }
-      
-        }
-        
+
+
+           if (Input.GetKeyDown(KeyCode.Mouse0))
+           {
+
+
+
+               Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+               Debug.DrawRay(ray.origin, ray.direction * 50, Color.yellow);
+               RaycastHit hit = new RaycastHit();
+               Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+               if (Physics.Raycast(ray, 20))
+               {
+                   print("Collided !!!");
+                   print(hitCounts);
+                   //           print( hit.collider.gameObject.tag);
+                   //           if (hit.collider.gameObject.tag == "Cubik")
+                   //             Destroy(cubik);
+                   score = score + 0.1f;
+                   hitCounts = hitCounts + 1;
+                   GameObject.FindGameObjectWithTag("ScoreCounter").GetComponent<Text>().text =
+                       "Score: " + Mathf.RoundToInt(score).ToString();
+                   GameObject.FindGameObjectWithTag("HitCounter").GetComponent<Text>().text =
+                       "Hits: " + hitCounts.ToString();
+
+               }
+           }
+       }
+
+
+
     }
 }
